a96adf542aa95f44f169303ca8f43d8e3161434d
babanila <babanila@yahoo.com>
2019-04-23 10:33:42 +0200

    feat(App.js): add navBar and inputForm to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
+import NavBar from "../src/components/navbar";
+import InputForm from "../src/components/inputForm";
 
 class App extends Component {
+  state = {
+    value: ""
+  };
+
+  handleInputChange = event => {
+    console.log(event.target.value);
+    this.setState({
+      value: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const sKey = this.state.value;
+    console.log(sKey);
+  };
+
   render() {
-    return <div> Hello World</div>;
+    return (
+      <div>
+        <NavBar />
+        <InputForm
+          onChange={this.handleInputChange}
+          onClick={this.handleSubmit}
+        />
+      </div>
+    );
   }
 }
 
