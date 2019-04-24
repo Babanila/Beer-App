c3945b6723d3e341568b5a19e87b2f19de22a410
babanila <babanila@yahoo.com>
2019-04-24 19:50:34 +0200

    refactor(inputForm.jsx): code refactor

--- src/components/inputForm.jsx
+++ src/components/inputForm.jsx
@@ -1,29 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 
-class InputForm extends Component {
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        <form onSubmit={e => this.props.onClick(e)}>
-          <h5> Please enter your Sandbox Key</h5>
-          <input
-            type="text"
-            className="navbar-brand w-50 mt-2 mb-2"
-            name="sandboxKey"
-            value={this.props.value}
-            onChange={e => this.props.onChange(e)}
-          />
-          <br />
-          <input
-            className="btn btn-primary btn-sm"
-            type="submit"
-            value="Submit"
-          />
-        </form>
-      </div>
-    );
-  }
-}
+// Stateless Functional Component
+const InputForm = props => {
+  const formHidden = props.hideForm ? "hidden" : "";
+
+  return (
+    <div className={formHidden}>
+      <form onSubmit={e => props.onSubmit(e)}>
+        <h5> {props.value !== "" && "Please enter your Sandbox Key"}</h5>
+
+        <input
+          type="text"
+          className="navbar-brand w-50 mt-2 mb-2"
+          name="sandboxKey"
+          value={props.value}
+          onChange={e => props.onChange(e)}
+        />
+        <br />
+        <input
+          className="btn btn-primary btn-sm"
+          type="submit"
+          value="Submit"
+        />
+      </form>
+    </div>
+  );
+};
 
 export default InputForm;
