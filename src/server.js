f33ee3d753f76332ed0aaa4cf9fa80afc8e1491a
babanila <babanila@yahoo.com>
2019-04-25 00:02:04 +0200

    feat(server.js): add url to get beer by id

--- src/server.js
+++ src/server.js
@@ -7,13 +7,28 @@ const app = express();
 // Setting CORS
 app.use(cors());
 
-// Using Sandbox key parameter to get the beer list
+// Using Sandbox key parameter to get the beer list.
 app.get("/:key", async (req, res) => {
   const url = `https://sandbox-api.brewerydb.com/v2/beers?key=${
     req.params.key
   }`;
   try {
     const { data } = await axios.get(url);
+    res.status(200).send(data);
+  } catch (e) {
+    res.status(e.response.status).send(e.response.statusText);
+  }
+});
+
+// Using Sandbox key and id parameter to get a particular beer.
+app.get("/:key/:id", async (req, res) => {
+  const url = `https://sandbox-api.brewerydb.com/v2/beer/${req.params.id}?key=${
+    req.params.key
+  }`;
+
+  try {
+    const result = await axios.get(url);
+    const { data } = result.data;
     res.status(200).send(data);
   } catch (e) {
     res.status(e.response.status).send(e.response.statusText);
