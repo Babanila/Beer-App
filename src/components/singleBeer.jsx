b2c5e08f8c1dd94639c1682d936c15108f0e17eb
babanila <babanila@yahoo.com>
2019-04-25 17:15:21 +0200

    feat(singleBeer.jsx): add single beer details

---
+++ src/components/singleBeer.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class SingleBeer extends Component {
+  state = {
+    singleBeer: {}
+  };
+
+  async componentDidUpdate(prevProps) {
+    if (this.props.keyValue === prevProps.keyValue) {
+      if (this.props.idValue !== prevProps.idValue) {
+        const url = `http://localhost:5000/${this.props.keyValue}/${
+          this.props.idValue
+        }`;
+        const result = await axios.get(url);
+        const { data } = result;
+        this.setState({
+          singleBeer: data
+        });
+      }
+    }
+  }
+
+  beerDetails = singleBeer =>
+    Object.entries(singleBeer).map(([k, value], i) => {
+      return typeof value !== "object" ? (
+        <div key={i} className="list-group-item">
+          <b>{k}</b>: {value}
+        </div>
+      ) : (
+        <div key={i} className="list-group-item">
+          <b>{k}</b>:{this.beerDetails(value)}
+        </div>
+      );
+    });
+
+  render() {
+    const beerDetailsHidden = this.props.hideBeerDetails ? "hidden" : "";
+
+    return (
+      <div className={beerDetailsHidden}>
+        <div className="list-group">
+          {this.beerDetails(this.state.singleBeer)}
+        </div>
+        <button
+          type="button"
+          className="btn btn-outline-dark btn-lg btn-block"
+          onClick={e => {
+            this.props.onClick(e);
+          }}
+        >
+          back
+        </button>
+      </div>
+    );
+  }
+}
+
+export default SingleBeer;
